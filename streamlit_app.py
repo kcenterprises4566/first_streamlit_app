eb137e0a2d5c469eb08cd0b0df961c67c77ac821
kcenterprises4566 <150215203+kcenterprises4566@users.noreply.github.com>
2023-11-15 14:37:27 -0600

    if else restructure

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,22 +26,25 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api repsonse
 streamlit.header("Fruityvice Fruit Advice!")
+try:
 #streamlit.text(fruityvice_response)
 #streamlit.text(fruityvice_response.json())
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  #streamlit.write('The user entered ', fruit_choice)
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get more information')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response)
+    #write your own comment -turns the json into a pandas datafram
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #write your own comment - displays the new dataframe as a streamlit table
+    streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.text(fruityvice_response)
-
-# write your own comment -turns the json into a pandas datafram
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - displays the new dataframe as a streamlit table
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
-
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
