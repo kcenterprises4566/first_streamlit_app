f613c6837588bca72f62182826d08702d017d046
kcenterprises4566 <150215203+kcenterprises4566@users.noreply.github.com>
2023-11-15 14:23:37 -0600

    secondary fruit entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,7 @@ my_data_rows = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#add second entry box
+add_ my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.text("Thanks for adding" + add_my_fruit)
